79d7a41aed12a06a6a95ada35815a8981e2d4461
Luke Price <39465652+LucasNPrice@users.noreply.github.com>
2019-05-21 17:10:46 -0400

    Add files via upload

--- run_fglasso.R
+++ run_fglasso.R
@@ -9,7 +9,7 @@
 source("sim_data.R")
 
 # simulated data: p x k x n array of simulated data.
-simdat <- sim_data(n = 100, p = 50, prec_model = 1)
+simdat <- sim_data(n = 100, p = 100, prec_model = 1)
 
 # turn 3D simulated data into list of 2D
 simList <- vector("list", dim(simdat)[1])
@@ -50,12 +50,52 @@ source("jFoldPCA.R")
 
 sample_cov <- pc_cov(fdobjList = fdSim, nharm = best_nharm, block = TRUE)
 
-#-----------------------------------------------------------#
-#     5. Begin fglasso algorithms                           #
-#-----------------------------------------------------------#
-source("fglasso2.R")
+#-----------------------------------------------------------------------------------------------#
+#     5. Begin fglasso algorithm                                                                #
+#-----------------------------------------------------------------------------------------------#
+source("fglasso4.R")
 
-fgm <- Algorithm_1(bmatrix = sample_cov, gamma = 100)
+# fgm <- fglasso(bmatrix = sample_cov, gamma = 90, cores = 3, doprint = TRUE, iterbreak = 20, iterlimit = 25)
+
+fgm_core1 <- fglasso(bmatrix = sample_cov, gamma = 110, cores = 1, doprint = TRUE, iterbreak = 20, iterlimit = 25) # failed at j=8 (g=110,p=100,n=100)
+fgm_core3 <- fglasso(bmatrix = sample_cov, gamma = 110, cores = 3, doprint = TRUE, iterbreak = 20, iterlimit = 25)
+
+
+# unblock_1 <- vector("list", 50)
+unblock_3 <- vector("list", 50)
+for (row in 1:50) {
+  
+  # unblock_1[[row]] <- do.call(cbind, fgm_core1$PrecisionMatrix[row,])
+  unblock_3[[row]] <- do.call(cbind, fgm_core3$PrecisionMatrix[row,])
+  
+}
+# unblock_1 <- do.call(rbind, unblock_1)
+unblock_3 <- do.call(rbind, unblock_3)
+
+# norm((unblock_1 - unblock_3), type = "F")
+
+simulated <- get_precision(modelnum = 1, p = 100)
+block_simulated <- block_matrix(inputmatrix = simulated, outersize = 50, innersize = best_nharm)
+
+# estimated_1 <- getEdges(blocked_matrix = fgm_core1$PrecisionMatrix)
+estimated_3 <- getEdges(blocked_matrix = fgm_core3$PrecisionMatrix)
+simulated_edges <- getEdges(blocked_matrix = block_simulated)
+
+# errRates1 <- getErrors(estimated_1, simulated_edges)
+errRates3 <- getErrors(estimated_1, simulated_edges)
+
+"Function:  15  /  50"
+
+ # nlesqlv & pracma both fail oat J = 39 for prec_mod = 1 on fglasso.3. (for parallel) (non-parallel fails at j = 4)
+
+# With gamma = 1:
+# Warning messages:
+#   1: In inv(A0) : Matrix appears to be singular.
+
+
+
+
+
 
 #-----------------------------------------------------------#
 #     5. Run fglasso with different gammas                  #
@@ -73,11 +113,6 @@ for (g in 1:length(gammas)) {
 }
 
 
-# how to set error criterion ---- what is a "good" difference between matrices and what is a "good" number of 
-# iterations meeting error criterion?
-
-# parallelize algorithms
-# bootstrap for optimal lambda 
 
 
 
