c0c175e9e32ef05e80e6b7ac15f19fd904c0119e
Luke Price <39465652+LucasNPrice@users.noreply.github.com>
2019-06-02 16:01:14 -0400

    Add files via upload

---
+++ run_fglasso.R
@@ -0,0 +1,63 @@
+# Scope: apply fglasso
+# load 'sim_data.R' to get data
+# load 'jFoldPCA.R' and run specified functions - fit fdobjs and obtain covariance matrix
+# load 'fglasso.R' and run fglasso() to obtain functional graphical model 
+
+#-----------------------------------------------------------#
+#     1. load/simulate data.                                #
+#-----------------------------------------------------------#
+source("sim_data.R")
+
+# simulated data: p x k x n array of simulated data.
+simdat <- sim_data(n = 100, p = 50, prec_model = 1)
+
+# turn 3D simulated data into list of 2D
+simList <- vector("list", dim(simdat)[1])
+for (j in 1:length(simList)) {
+  simList[[j]] <- simdat[j,,]
+}
+
+#-----------------------------------------------------------#
+#     2. determine best number of basis functions L.        #
+#-----------------------------------------------------------#
+source("jFoldPCA.R")
+
+simFit <- fit_Basis(dataList = simList, nbasistry = 5:20)
+plot(simFit$gcvs)
+
+best_basis = simFit$best.basis
+fdSim <- fit_Basis(dataList = simList, nbasistry = best_basis)$fdobjList
+
+#-----------------------------------------------------------#
+#     3. determine best number of harmonics by              #
+#         performing J-fold cross-validation.               #
+#-----------------------------------------------------------#
+source("jFoldPCA.R")
+
+cvpca <- jFoldPCA(dataList = simList, nbasis = best_basis, 
+                  nfolds = 4, nharmrange = 1:best_basis)
+plot(cvpca$nharm_sse)
+best_nharm <- cvpca$best_nharm
+
+#-----------------------------------------------------------#
+#     3. Get sample covariance matrix of eigenscores        #
+#         (using best_nharm)                                #
+#         block the covariance matrix                       #
+#-----------------------------------------------------------#
+source("jFoldPCA.R")
+
+# centerfns = TRUE or FALSE?
+
+sample_cov <- pc_cov(fdobjList = fdSim, nharm = best_nharm, block = TRUE)
+
+
+#-----------------------------------------------------------------------------------------------#
+#     5. Begin fglasso algorithm                                                                #
+#-----------------------------------------------------------------------------------------------#
+source("fglasso4.R")
+source("Edges.R")
+
+fg_obj <- fglasso(bmatrix = sample_cov, gamma = 90, cores = 3, doprint = TRUE, iterbreak = 20, iterlimit = 25)
+est_edges <- getEdges(fgm$PrecisionMatrix)
+
+
